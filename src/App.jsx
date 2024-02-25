058b21bf84dfb5a66c00e6c2db83e85e9f07b97f
Paweł Gargula <pawel.gargula@interia.pl>
2024-02-25 12:23:05 +0100

    Add google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css'
+import './App.css';
+import InvestmentChart from './components/investment-chart';
+import { useState } from 'react';
 
 function App() {
+  const [annualPercentageRate, setAnnualPercentageRate] = useState(6);
+  const [depositsWithinOneYear, setDepositsWithinOneYear] = useState(4);
+  const [onetimeDeposit, setOnetimeDeposit] = useState(1400);
+  const [depositFee, setDepositFee] = useState(14);
+  const [investmentDurationInYear, setInvestmentDurationInYear] = useState(10);
+  const [initialDeposit, setInitialDeposit] = useState(0);
+
+  const percentageRate = (annualPercentageRate + 100) / 100;
+  const totalNumberOfDeposits = (depositsWithinOneYear || 1) * investmentDurationInYear;
+  const oneDepositRate = Math.pow(percentageRate, 1 / (depositsWithinOneYear || 1));
+
+  let sum = initialDeposit;
+  for (let i = 1; i <= totalNumberOfDeposits; i++) {
+      sum += onetimeDeposit;
+      sum = sum * oneDepositRate;
+  }
+
+  const allPaymentFee = depositFee * depositsWithinOneYear * investmentDurationInYear || depositFee;
+  const allInputs = onetimeDeposit * depositsWithinOneYear * investmentDurationInYear + allPaymentFee + initialDeposit;
+  const allInputsString = `${allInputs} (${allPaymentFee} is fee)`;
+
+  const profit = sum - allInputs;
+  const profitAfterTaxPayment = profit * 0.81;
+
   return (
     <>
       <h1>Investment calculator</h1>
+      <InvestmentChart />
     </>
   )
 }
